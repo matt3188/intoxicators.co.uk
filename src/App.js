9630fa2813b55bb59be476172d2428567de2c1d6
Matt Coleman <matt.coleman@protonmail.com>
2018-01-13 16:58:23 +0000

    Removed logo and changed text

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.jpg";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <section className="App-section">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <p className="App-intro">Website coming soon</p>
+        </section>
       </div>
     );
   }

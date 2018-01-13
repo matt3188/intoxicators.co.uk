ab8024bb4363121ed46b4a08687fdd0d76f5363b
Matt Coleman <matt.coleman@protonmail.com>
2018-01-13 21:19:28 +0000

    Added Big calendar and linked to Google calendar

--- src/Components/Calendar.js
+++ src/Components/Calendar.js
@@ -1,10 +1,26 @@
 import React, { Component } from "react";
+import moment from "moment";
+import BigCalendar from "react-big-calendar";
+import { getEvents } from "../Services/gcal";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+// a localizer for BigCalendar
+BigCalendar.momentLocalizer(moment);
+
 class Calendar extends Component {
   constructor() {
     super();
+    this.state = {
+      events: []
     };
   }
+  componentDidMount() {
+    getEvents(events => {
+      this.setState({ events });
+    });
+  }
   render() {
+    return <BigCalendar events={this.state.events} />;
   }
 }
 

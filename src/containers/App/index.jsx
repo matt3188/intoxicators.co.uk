1d61804e3fbace1b5a75e8a861d4a20936f094c7
Matt Coleman <matt.coleman@protonmail.com>
2018-01-21 00:54:36 +0000

    Converted App container into a functional component

--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -1,25 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Home from 'containers/Home';
-import Calendar from 'containers/Calendar';
+
 import Header from 'components/Header';
 import Footer from 'components/Footer';
 
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <div className="app">
-          <Header />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/calendar" component={Calendar} />
-          </Switch>
-          <Footer />
-        </div>
-      </BrowserRouter>
-    );
-  }
-}
+import HomePage from 'containers/HomePage';
+import CalendarPage from 'containers/CalendarPage';
+
+const App = () => (
+  <BrowserRouter>
+    <div className="app">
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/calendar" component={CalendarPage} />
+      </Switch>
+      <Footer />
+    </div>
+  </BrowserRouter>
+);
 
 export default App;

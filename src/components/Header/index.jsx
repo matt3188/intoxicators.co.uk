4d1143aaaf9c556ee74a51b3961c4b5c2fd58fef
Matt Coleman <matt.coleman@protonmail.com>
2018-01-29 20:12:10 +0000

    Changed alt tag

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
 import Navigation from 'components/Navigation';
 import logo from 'images/text-logo.png';
+
 import './header.css';
 
 const Header = () => (
   <div className="header">
     <Link to="/">
-      <img src={logo} className="text-logo" alt="logo" />
+      <img src={logo} className="text-logo" alt="Intoxicators logo" />
     </Link>
 
     <Navigation />

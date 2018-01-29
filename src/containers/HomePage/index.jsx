7b73ef913bb5fc26fdee8fbcc10886b091da277b
Matt Coleman <matt.coleman@protonmail.com>
2018-01-29 20:11:23 +0000

    Added grid to homepage

--- src/containers/HomePage/index.jsx
+++ src/containers/HomePage/index.jsx
@@ -3,14 +3,28 @@ import Carousel from 'components/Carousel';
 import Card from 'components/Card';
 import './home.css';
 
+import { Grid, Row, Col } from 'react-bootstrap';
+
 const Home = () => (
-  <div className="home">
-    <section className="section">
-      <Carousel />
-      <Card title="Upcoming events" image="images/logo.jpg" linkTo="/calendar" />
-      <Card title="Our cars" image="images/slider-03.jpg" linkTo="/calendar" />
-      <Card title="Join the club" image="images/slider-04.jpg" linkTo="/calendar" />
-    </section>
+  <div className="homepage">
+    <Carousel />
+    <Grid>
+      <Row className="show-grid">
+        <div className="home">
+          <section className="section">
+            <Col sm={6} md={4}>
+              <Card title="Upcoming events" image="images/logo.jpg" linkTo="/calendar" />
+            </Col>
+            <Col sm={6} md={4}>
+              <Card title="Our Members" image="images/slider-03.jpg" linkTo="/calendar" />
+            </Col>
+            <Col sm={6} md={4}>
+              <Card title="Join the club" image="images/slider-04.jpg" linkTo="/calendar" />
+            </Col>
+          </section>
+        </div>
+      </Row>
+    </Grid>
   </div>
 );
 

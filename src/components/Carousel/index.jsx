052b4c3de8138604ab3499c64bbe110021a7c2c7
Matt Coleman <matt.coleman@protonmail.com>
2018-01-29 20:11:11 +0000

    Syntax changes

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,35 +1,36 @@
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import "./carousel.css";
-import React, { Component } from "react";
-import Slider from "react-slick";
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import './carousel.css';
+import React, { Component } from 'react';
+
+import Slider from 'react-slick';
 
 class slickCarousel extends Component {
   render() {
     const objects = [
       {
-        image: "images/slider-01.jpg",
-        title: "title 01"
+        image: 'images/slider-01.jpg',
+        title: 'title 01'
       },
       {
-        image: "images/slider-02.jpg",
-        title: "title 02"
+        image: 'images/slider-02.jpg',
+        title: 'title 02'
       },
       {
-        image: "images/slider-03.jpg",
-        title: "title 03"
+        image: 'images/slider-03.jpg',
+        title: 'title 03'
       },
       {
-        image: "images/slider-04.jpg",
-        title: "title 04"
+        image: 'images/slider-04.jpg',
+        title: 'title 04'
       },
       {
-        image: "images/slider-05.jpg",
-        title: "title 05"
+        image: 'images/slider-05.jpg',
+        title: 'title 05'
       },
       {
-        image: "images/slider-06.jpg",
-        title: "title 06"
+        image: 'images/slider-06.jpg',
+        title: 'title 06'
       }
     ];
 
@@ -48,12 +49,7 @@ class slickCarousel extends Component {
         <div className="bg-tint" />
         <Slider className="slider" {...settings}>
           {objects.map(item => (
-            <li
-              key={item.title}
-              className="slide"
-              style={{ backgroundImage: `url(${  item.image  })` }}
-              alt={item.title}
-            />
+            <li key={item.title} className="slide" style={{ backgroundImage: `url(${item.image})` }} alt={item.title} />
           ))}
         </Slider>
       </div>
--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./navigation.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './navigation.css';
 
 const Navigation = () => (
   <ul className="navigation">

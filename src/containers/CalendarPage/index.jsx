17086d19116f85cbd8b1bd8dacb7b0e58d3e01fa
Matt Coleman <matt.coleman@protonmail.com>
2018-01-29 21:04:19 +0000

    Wrapped calendar in bootstrap grid

--- src/containers/CalendarPage/index.jsx
+++ src/containers/CalendarPage/index.jsx
@@ -2,13 +2,18 @@ import React, { Component } from 'react';
 import './calendar.css';
 
 import Calendar from 'components/Calendar';
+import { Grid, Row, Col } from 'react-bootstrap';
 
 class CalendarPage extends Component {
   render() {
     return (
-      <div>
-        <Calendar />
-      </div>
+      <Grid>
+        <Row className="show-grid">
+          <Col lg={12}>
+            <Calendar />
+          </Col>
+        </Row>
+      </Grid>
     );
   }
 }

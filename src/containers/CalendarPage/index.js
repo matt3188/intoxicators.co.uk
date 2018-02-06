aa173b42b31f5687c8096597856a5e29bd60c3eb
Matt Coleman <matt.coleman@protonmail.com>
2018-02-06 14:48:49 +0000

    Got rid of return statement

--- src/containers/CalendarPage/index.js
+++ src/containers/CalendarPage/index.js
@@ -4,16 +4,14 @@ import { Grid, Row, Col } from 'react-bootstrap';
 import Calendar from 'components/Calendar';
 import './calendar.css';
 
-const CalendarPage = () => {
-  return (
-    <Grid>
-      <Row className="show-grid">
-        <Col lg={12}>
-          <Calendar />
-        </Col>
-      </Row>
-    </Grid>
-  );
-};
+const CalendarPage = () => (
+  <Grid>
+    <Row className="show-grid">
+      <Col lg={12}>
+        <Calendar />
+      </Col>
+    </Row>
+  </Grid>
+);
 
 export default CalendarPage;

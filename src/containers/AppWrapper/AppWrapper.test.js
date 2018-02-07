6105bac6714a382d1d957f93bd5bd48f91d12c16
Matt Coleman <matt.coleman@protonmail.com>
2018-02-07 11:58:45 +0000

    Deleted old test

--- src/containers/AppWrapper/AppWrapper.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AppWrapper from '../AppWrapper';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<AppWrapper />, div);
-});

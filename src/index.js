818dad410101b602f29e4cf533c5201370b9cf8a
Matt Coleman <matt.coleman@protonmail.com>
2018-02-16 16:13:37 +0000

    Changed store import name to configureStore

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
-import store, { history } from './redux/store';
+import configureStore, { history } from './redux/store';
 
 import AppWrapper from './containers/AppWrapper/AppWrapper';
 
@@ -10,6 +10,7 @@ import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 const rootElement = document.getElementById('root');
+const store = configureStore();
 
 render(
   <Provider store={store}>

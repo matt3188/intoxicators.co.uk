965af63eea047586bbcd34a6c1e145a3bfed8f41
Matt Coleman <matt.coleman@thisisdare.com>
2018-03-08 12:57:31 +0000

    Wrote test for HeaderView

---
+++ src/components/Header/HeaderView.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import HeaderView from './HeaderView';
+
+describe('(Component) <Header />', () => {
+  it('should render the correct title', () => {
+    const wrapper = shallow(
+      <HeaderView
+        authService={{}}
+        history={{ push: () => null }}
+        auth={{ isAuthenticated: false }}
+        loginRequest={() => null}
+        logoutSuccess={() => null}
+      />
+    );
+  });
+});

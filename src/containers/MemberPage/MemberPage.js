9f8bce180e0c92c3e305efebf2a39770f8298770
Matt Coleman <matt.coleman@thisisdare.com>
2018-03-08 15:52:59 +0000

    Cleaned up members section

--- src/containers/MemberPage/MemberPage.js
+++ src/containers/MemberPage/MemberPage.js
@@ -1,12 +1,8 @@
 import React, { Component } from 'react';
 
 const members = JSON.parse(localStorage.getItem('members'));
-console.log(members);
 
 class MemberPage extends Component {
-  componentDidMount() {
-    const { match: { params } } = this.props;
-  }
   render() {
     return <div>{members.map(owner => <div key={owner.name}>{owner.vehicle}</div>)}</div>;
   }
